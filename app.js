b9313beeee79d9846681fd9baedd0f5bbcb5a761
RAalders <raalders@boomerweb.nl>
2017-11-12 19:31:28 +0100

    tested playmusic lib.

--- app.js
+++ app.js
@@ -1,51 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var sassMiddleware = require('node-sass-middleware');
+const express = require('express');
+const app = express();
+const socketio = require('socket.io');
 
-var index = require('./routes/index');
+const args = require('args-parser')(process.argv);
+const promisify = require('promisify-node');
+const PlayMusic = promisify('playmusic');
+const pm = new PlayMusic();
 
-var app = express();
+if(!args.email || !args.password) {
+    console.error('Email of password missing. correct usage: node app.js --email=EMAIL --password=PASSWORD');
+    return;
+}
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//
+// async function run() {
+//     await pm.init({email: args.email, password: args.password});
+//     const library = await pm.getAllTracks();
+//     const storeSongs = library.data.items;
+//     console.dir(storeSongs);
+// }
+//
+// run().catch(error => console.error(error));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', index);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function (req, res) {
+    res.send('hello world')
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+const io = socketio.listen(app.listen(3000));
